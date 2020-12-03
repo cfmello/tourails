b1b1520d399ed8fcfef435e505351316e9a2226d
Adonay Veiga <adonayveiga@gmail.com>
2020-12-03 16:03:47 -0300

    A user can book a service

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,12 +1,11 @@
 class BookingValidator < ActiveModel::Validator
   def validate(record)
-    # return unless record.user == record.user
     if record.offer && record.user == record.offer.user
       record.errors[:offer] << "Can't be user and guide simultaneously"
     elsif record.date_start && record.date_start < Time.now
       record.errors[:date_start] << "Start day must be in the future"
     elsif record.date_end && record.date_end < record.date_start
-      record.errors[:date_start] << "End day must be greater then start day"
+      record.errors[:date_end] << "End day must be greater then start day"
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,18 @@ ActiveRecord::Schema.define(version: 2020_12_03_151345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "date_start"
+    t.date "date_end"
+    t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
+    t.text "special_request"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["offer_id"], name: "index_bookings_on_offer_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "offers", force: :cascade do |t|
     t.string "city"
     t.text "description"
@@ -22,16 +34,6 @@ ActiveRecord::Schema.define(version: 2020_12_03_151345) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_offers_on_user_id"
-  end
-  
-  create_table "bookings", force: :cascade do |t|
-    t.date "date_start"
-    t.date "date_end"
-    t.bigint "user_id", null: false
-    t.text "special_request"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_151345) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "offers", "users"
+  add_foreign_key "bookings", "offers"
   add_foreign_key "bookings", "users"
+  add_foreign_key "offers", "users"
 end
