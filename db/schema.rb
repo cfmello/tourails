375522436bdfa99970f87c05510ec225368ecf7c
Cristhiano Mello <cristhiano.f.mello@gmail.com>
2020-12-03 17:17:17 -0300

    Fixed booking DB

--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 2020_12_03_175421) do
     t.date "date_start"
     t.date "date_end"
     t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
     t.text "special_request"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["offer_id"], name: "index_bookings_on_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
@@ -51,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_175421) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "offers"
   add_foreign_key "bookings", "users"
   add_foreign_key "offers", "users"
 end
