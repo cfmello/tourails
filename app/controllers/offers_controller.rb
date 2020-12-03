0679296c08651bf588569c2f60f4d5b0a73c958b
Cristhiano Mello <cristhiano.f.mello@gmail.com>
2020-12-03 14:12:48 -0300

    Created offer model and controller with new/create and destroy

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,35 @@
+class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user_id = current_user.id
+    if @offer.save
+      redirect_to offers_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    redirect_to offers_path
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:city, :description)
+  end
+end
