cd218a63a181025a51a552f83d73bb376ee58f07
Adonay Veiga <adonayveiga@gmail.com>
2020-12-03 14:11:34 -0300

    Booking model created and validated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_202358) do
+ActiveRecord::Schema.define(version: 2020_12_03_151345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "date_start"
+    t.date "date_end"
+    t.bigint "user_id", null: false
+    t.text "special_request"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_12_02_202358) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
 end
