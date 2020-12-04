98a512ad3176d3d94757926d1eaf8cc9cf66566f
Cristhiano Mello <cristhiano.f.mello@gmail.com>
2020-12-04 16:47:20 -0300

    Separa minhas ofertas das outras e inativação/ativação

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,15 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.all
+    @offers = []
+    @myoffers = []
+    all_offers = Offer.all
+    all_offers.each do |offer|
+      if offer.user != current_user && offer.active
+        @offers << offer
+      elsif offer.user == current_user
+        @myoffers << offer
+      end
+    end
   end
 
   def show
@@ -23,13 +32,19 @@ class OffersController < ApplicationController
 
   def destroy
     @offer = Offer.find(params[:id])
-    @offer.destroy
-    redirect_to offers_path
+    act = true
+    act = false if @offer.active
+    @offer.active = act
+    if @offer.save
+      redirect_to offers_path
+    else
+      render action: "show", id: @offer.id
+    end
   end
 
   private
 
   def offer_params
-    params.require(:offer).permit(:city, :description)
+    params.require(:offer).permit(:city, :description, :active)
   end
 end
