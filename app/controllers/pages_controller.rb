3d420933f763aeb59c5346cdcf176f135c28fa92
Adonay Veiga <adonayveiga@gmail.com>
2020-12-07 14:49:17 -0300

    Melhorias na UX para agendamento a partir da pesquisa

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,9 +5,10 @@ class PagesController < ApplicationController
   end
 
   def search
-    if params[:query]
+    if params[:query].present?
       @results = Offer.ominisearch(params[:query])
     else
+      # If query is empty, return last ten included offers
       @results = Offer.last(10)
     end
   end
