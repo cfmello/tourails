75517c70fbb67a055920799d4f131b4c998d57e8
Adonay Veiga <adonayveiga@gmail.com>
2020-12-08 12:22:01 -0300

    Ofertas na home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home search]
 
   def home
+    @results = Offer.where(active: true).last(3)
   end
 
   def search
     if params[:query].present?
-      @results = Offer.ominisearch(params[:query])
+      @results = Offer.ominisearch(params[:query]).where(active: true)
     else
       # If query is empty, return last ten included offers
       @results = Offer.last(10)
